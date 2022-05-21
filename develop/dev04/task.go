be97dbfd89f1006876e1d2d44eb505ecb3d7370b
mv <mv@outsi.de>
2022-05-21 18:00:29 +0300

    feat: add dev04

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strings"
+)
+
 /*
 === Поиск анаграмм по словарю ===
 
@@ -19,6 +28,89 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
+//sortWord - returns the word sorted alphabetically
+func sortWord(word string) string {
+	arr := []rune(word)
+	sort.Slice(arr, func(i, j int) bool { return arr[i] < arr[j] })
+	return string(arr)
+} 
+
+//sortStringSlice - returns words sorted alphabetically
+func sortStringSlice(strs []string) []string {
+	sort.Slice(strs, func(i, j int) bool { return strs[i] < strs[j] })
+	return strs
+}
+
+//removeDuplicates - removes all duplicates in an array 
+func removeDuplicates(strs []string) []string {
+	if len(strs) == 0 {
+		return nil
+	}
+	var res []string
+	for i := 1; i < len(strs); i++ {
+		if strs[i] != strs[i - 1] {
+			res = append(res, strs[i - 1])
+		}
+	}
+	res = append(res, strs[len(strs) - 1])
+	return res
+}
+
+//SearchAnagrams - searches for all anagrams in an array
+func SearchAnagrams(vocabulary []string) map[string][]string {
+
+	mapDuplicate := make(map[string][]int)
+	mapResult := make(map[string][]string)
+
+	//sort words by letters and memorize their indexes
+	for i, word := range vocabulary {
+		sWord := sortWord(word)
+		mapDuplicate[sWord] = append(mapDuplicate[sWord], i)
+	}
+
+	for mapDupK := range mapDuplicate {
+		if len(mapDuplicate[mapDupK]) > 1 {
+			for _, numberWord := range mapDuplicate[mapDupK] {
+				mapResult[vocabulary[mapDuplicate[mapDupK][0]]] = append(mapResult[vocabulary[mapDuplicate[mapDupK][0]]], vocabulary[numberWord])
+			}
+		}
+	}
+
+
+	//sort and remove duplicates
+	for mapR := range mapResult {
+		mapResult[mapR] = sortStringSlice(mapResult[mapR])
+		mapResult[mapR] = removeDuplicates(mapResult[mapR])
+	}
+
+	return mapResult
+}
+
 func main() {
+
+	//open file
+	file, err := os.Open("anagrams.data")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var vocabulary []string
+
+	//read file line by line
+	fileScanner := bufio.NewScanner(file)
+	for fileScanner.Scan() {
+		vocabulary = append(vocabulary, strings.ToLower(fileScanner.Text()))
+	}
+
+	if err := fileScanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(vocabulary)
+
+	anagrams := SearchAnagrams(vocabulary)
+
+	fmt.Println(anagrams)
 
 }
--- develop/dev04/task_test.go
+++ develop/dev04/task_test.go
@@ -1,1 +1,125 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain_sortWord(t *testing.T) {
+	
+	testTable := []struct {
+		name string
+		input string
+		output string
+	} {
+		{
+			name: "OK",
+			input: "столик",
+			output: "иклост",
+		},
+		{
+			name: "Empty",
+			input: "",
+			output: "",
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			res := sortWord(testCase.input)
+			assert.Equal(t, testCase.output, res)
+		})
+	}
+
+}
+
+func TestMain_sortStringSlice(t *testing.T) {
+	
+	testTable := []struct {
+		name string
+		input []string
+		output []string
+	} {
+		{
+			name: "OK",
+			input: []string{"столик", "слоник", "дом"},
+			output: []string{"дом", "слоник", "столик"},
+		},
+		{
+			name: "Empty",
+			input: []string{""},
+			output: []string{""},
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			res := sortStringSlice(testCase.input)
+			assert.Equal(t, testCase.output, res)
+		})
+	}
+	
+}
+
+func TestMain_removeDuplicates(t *testing.T) {
+	
+	testTable := []struct {
+		name string
+		input []string
+		output []string
+	} {
+		{
+			name: "OK",
+			input: []string{"столик", "столик", "слоник", "дом"},
+			output: []string{"столик", "слоник", "дом"},
+		},
+		{
+			name: "Empty",
+			input: []string{""},
+			output: []string{""},
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			res := removeDuplicates(testCase.input)
+			assert.Equal(t, testCase.output, res)
+		})
+	}
+	
+}
+
+
+func TestMain_SearchAnagrams(t *testing.T) {
+
+	outputTrue := make(map[string][]string)
+	outputTrue["листок"] = append(outputTrue["листок"], "листок")
+	outputTrue["листок"] = append(outputTrue["листок"], "столик")
+	
+	testTable := []struct {
+		name string
+		input []string
+		output map[string][]string
+	} {
+		{
+			name: "OK",
+			input: []string{"листок", "столик", "столик", "слоник", "дом"},
+			output: outputTrue,
+		},
+		{
+			name: "Empty",
+			input: []string{""},
+			output: make(map[string][]string),
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			res := SearchAnagrams(testCase.input)
+			assert.Equal(t, testCase.output, res)
+		})
+	}
+	
+}
+
