55efb69e5900a920573055dbfa610145b28d0fa9
mv <mv@outsi.de>
2022-05-28 01:22:21 +0300

    feat: add dev10

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
 /*
 === Утилита telnet ===
 
@@ -15,6 +26,64 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 При подключении к несуществующему сервер, программа должна завершаться через timeout.
 */
 
-func main() {
+//Scanner - read from Stdin and write to net.Conn
+func Scanner(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	scanner := bufio.NewScanner(os.Stdin)
 
+	for scanner.Scan() {
+		_, err := conn.Write([]byte(scanner.Text() + "\n"))
+		//_, err := fmt.Fprint(conn, scanner.Text() + "\n")
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+	defer conn.Close() // if ctrl + D
+}
+
+//Reader - read from net.Conn and write to Stdout
+func Reader(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	reader := bufio.NewReader(conn)
+
+	for {
+		message, err := reader.ReadString(' ')
+		if err == io.EOF { // if server closed connection
+			fmt.Println("\nConnection is closed")
+			os.Exit(0)
+		}
+		if netErr, ok := err.(net.Error); ok && !netErr.Timeout() { // if Scanner closed connection
+			break
+		}
+		fmt.Fprint(os.Stdout, message)
+	}
+}
+
+//For example - freechess.org:5000
+func main() {
+	timeout := flag.Duration("timeout", time.Second * 10, "Timeout")
+	flag.Parse()
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+
+	fmt.Println("Timeout:", timeout, "Host:", host, "Port:", port)
+
+	conn, err :=  net.DialTimeout("tcp", host + ":" + port, *timeout)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	countWorkers := 2
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(countWorkers)
+	go Reader(conn, &wg)
+	go Scanner(conn, &wg)
+
+	wg.Wait()
+
+	defer fmt.Println("\nClose program")
 }
