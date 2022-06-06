e068f208af92e5204fa7444f0fa059d91284589e
mv <mv@outsi.de>
2022-06-06 18:33:38 +0300

    feat: add dev06

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +22,60 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func delimiter(strs []string, field int, delim string, sep bool) ([]string, error) {
+	var res []string
+	var temp []string
 
+	for _, val := range strs {
+		temp = strings.Split(val, delim)
+		if sep && len(temp) < 2 {
+			continue
+		}
+		if (len(temp) >= field) {
+			res = append(res, temp[field - 1])
+		}
+	}
+
+	return res, nil
+}
+
+func print(strs []string) {
+	for _, val := range strs {
+		fmt.Println(val) 
+	}
+}
+
+func main() {
+	fFlag := flag.Int("f", 1, "fields")
+	dFlag := flag.String("d", "\t", "delimiter")
+	sFlag := flag.Bool("s", false,  "separated")
+	flag.Parse()
+
+	fileName := flag.Arg(0)
+
+	//open file
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var strs []string
+
+	//read file line by line
+	fileScanner := bufio.NewScanner(file)
+	for fileScanner.Scan() {
+		strs = append(strs, fileScanner.Text())
+	}
+
+	if err := fileScanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := delimiter(strs, *fFlag, *dFlag, *sFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	print(result)
 }
--- develop/dev06/task_test.go
+++ develop/dev06/task_test.go
@@ -1,1 +1,69 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain_delimiter(t *testing.T) {
+
+	strs := []string{"Winter: white: snow: frost",
+					"Spring: green: grass: warm",
+					"Summer: colorful: blossom: hot",
+					"Autumn: yellow: leaves: cool",
+					"asdsadasdasd"}
+
+	res1 := []string{"Winter",
+					"Spring",
+					"Summer",
+					"Autumn",}
+
+	res2 := []string{"Winter",
+					"Spring",
+					"Summer",
+					"Autumn",
+					"asdsadasdasd"}
+	
+	testTable := []struct {
+		name string
+		input []string
+		output []string
+		field int
+		delim string
+		sep bool
+	} {
+		{
+			name: "All options",
+			input: strs,
+			output: res1,
+			field: 1,
+			delim: ":",
+			sep: true,
+		},
+		{
+			name: "Default delimiter",
+			input: strs,
+			output: strs,
+			field: 1,
+			delim: "\t",
+			sep: false,
+		},
+		{
+			name: "No separated",
+			input: strs,
+			output: res2,
+			field: 1,
+			delim: ":",
+			sep: false,
+		}, 
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			res, _ := delimiter(testCase.input, testCase.field, testCase.delim, testCase.sep)
+			assert.Equal(t, testCase.output, res)
+		})
+	} 
+
+}
